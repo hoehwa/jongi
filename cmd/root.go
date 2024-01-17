cd3df7ff4ba06a3f0186c130d905ba6d14c38212
mindulle <mindullestudio@gmail.com>
2024-01-17 13:28:58 +0900

    :bookmark: Add version

--- cmd/root.go
+++ cmd/root.go
@@ -15,8 +15,9 @@ import (
 
 // RootCmd represents the base command when called without any subcommands.
 var RootCmd = &cobra.Command{
-	Use:   "jongi",
-	Short: "A brief description of your application",
+	Use:     "jongi",
+	Version: "v0.1.4",
+	Short:   "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
